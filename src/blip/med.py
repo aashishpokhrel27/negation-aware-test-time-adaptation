3623c18ab4e82add0f42d619529e158e649cd892
Aashish Pokhrel <ashishpokhrel27@gmail.com>
2026-01-22 13:35:47 -0700

    update blip

--- src/blip/med.py
+++ src/blip/med.py
@@ -38,11 +38,13 @@ from transformers.modeling_outputs import (
 )
 from transformers.modeling_utils import (
     PreTrainedModel,
-    find_pruneable_heads_and_indices,
     prune_linear_layer,
 )
 
-from transformers.pytorch_utils import apply_chunking_to_forward
+from transformers.pytorch_utils import (
+    apply_chunking_to_forward,
+    find_pruneable_heads_and_indices
+)
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
